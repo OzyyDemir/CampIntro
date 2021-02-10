53b8849a269ec0c7cc97011d2b87947115b40ca9
OzyyDemir <oguzhandmr98@gmail.com>
2021-02-11 00:16:59 +0300

    oopstaff

---
+++ oop1/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Product added : "+product.ProductName);
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine("Product updated : " + product.ProductName);
+        }
+
+        public void Delete(Product product)
+        {
+            Console.WriteLine("Product deleted : " + product.ProductName);
+        }
+
+    }
+}
---
+++ oop1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Oop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Table";
+            product1.UnitPrice = 44;
+            product1.UnitsInStock = 33;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitsInStock = 55,
+                UnitPrice = 12,
+                ProductName = "Pencil"
+            };
+
+            ProductManager productManager = new ProductManager();
+
+            productManager.Add(product2);
+
+        }
+    }
+}
