850ce47f179ac2ba12e6a996e4581f9248a12c1e
OzyyDemir <oguzhandmr98@gmail.com>
2021-02-10 23:29:32 +0300

    CustomerManager

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Costumer added :" +customer.Name);
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Costumer updated :" + customer.Name);
+
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Costumer deleted :" + customer.Name);
+
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Ali";
+            customer1.CustomerNo = 123123;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Merve";
+            customer2.CustomerNo = 123134;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Murat";
+            customer3.CustomerNo = 131131;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            customerManager.Update(customer1);
+            customerManager.Delete(customer1);
+
+        }
+    }
+}
